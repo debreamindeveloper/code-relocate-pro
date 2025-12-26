848aa32d9d5766fc20aa49b758b1fa6e0d704ce5
Eskindir <eskindir.abdela@gmail.com>
2025-12-26 22:52:12 +0200

    meta tags and language edits

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,8 +8,23 @@ import Map from "@/components/Map";
 import Contact from "@/components/Contact";
 import Donate from "@/components/Donate";
 import Footer from "@/components/Footer";
+import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
 
 const Index = () => {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    // Update document title based on selected language
+    const titles = {
+      am: "በፊንላንድ የሄልሲንኪ ደብረ አሚን አቡነ ተክለሃይማኖት ቤተ ክርስቲያን",
+      en: "Debre Amin Abune Teklehaymanot Church, Helsinki, Finland",
+      fi: "Helsingin Debre Amin Abune Teklehaymanot Kirkko",
+    };
+
+    document.title = titles[i18n.language as keyof typeof titles] || titles.en;
+  }, [i18n.language]);
+
   return (
     <div className="min-h-screen">
       <Header />
